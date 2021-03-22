085932d6a39e352d3483fa12c3a28c252d6b8999
Joao Munoz <joao.munoz@mercadoeletronico.eu>
2021-03-22 18:54:45 +0000

    apiv4

--- lambda/index.js
+++ lambda/index.js
@@ -15,7 +15,7 @@ const IsRequestsHandler = {
         const {data} = await axios.get("https://me-alexa-api.herokuapp.com/orders") 
 
         return handlerInput.responseBuilder
-            .speak("1")
+            .speak(JSON.stringify(data))
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
